40e5c96cf82377be45320498aaa3e040cfe2de6d
Huseyin BABAL <huseyin.babal@aurea.com>
2017-08-22 07:05:20 +0300

    Initial project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import {AngularFireModule} from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+import {FormsModule} from "@angular/forms";
+
+export const firebaseConfig = {
+  apiKey: 'AIzaSyC18sqDSDTv8Qla28WPk4XZnmyTfy5Wcyk',
+  authDomain: 'huseyin-babal.firebaseapp.com',
+  databaseURL: 'https://huseyin-babal.firebaseio.com',
+  projectId: 'huseyin-babal',
+  storageBucket: 'huseyin-babal.appspot.com',
+  messagingSenderId: '128896541689'
+};
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
   ],
   providers: [],
   bootstrap: [AppComponent]
